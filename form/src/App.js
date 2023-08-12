89fcbb969df5816a36377df183d909a1a1924c84
Abhinav Singh <kittusinghranchi@gmail.com>
2023-08-12 16:52:45 +0530

    navbar ready

--- form/src/App.js
+++ form/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import "./App.css";
 import Navbar from './components/Navbar';
+import MainForm from './components/MainForm';
 const App = () => {
   return (
     <div className=''>
       <Navbar/>
+      <MainForm/>
     </div>
   )
 }
--- form/src/components/Navbar.js
+++ form/src/components/Navbar.js
@@ -1,11 +1,36 @@
-import React from 'react'
-
+import React from "react";
+import { AiOutlineUser } from "react-icons/ai";
+import { ReactComponent as Logo } from "../Assets/RawSvg/logo.svg";
+import { Avatar, AvatarBadge, AvatarGroup, Button } from "@chakra-ui/react";
+import {HiOutlineMail} from "react-icons/hi";
 const Navbar = () => {
   return (
-    <div className=''>
-      
+    <div className="flex flex-row justify-between h-[15vh] items-center ml-[2%] mr-[3%]">
+      <div className="flex-row flex">
+        <div>
+          <Logo className="w-[5vw] h-[10vw]" />
+        </div>
+        <div className="flex flex-row items-center justify-center text-2xl font-ubuntu">
+          AffWorld Technologies
+        </div>
+      </div>
+      <div className="flex flex-row justify-between items-center gap-4">
+        <div>
+          <Button leftIcon={<HiOutlineMail />} colorScheme="red" variant="solid">
+            Email Us
+          </Button>
+        </div>
+        <div>
+          <Button colorScheme="red">Sign In</Button>
+        </div>
+        <div>
+          <AvatarGroup spacing="1rem">
+            <Avatar bg="red.500" icon={<AiOutlineUser fontSize="1.5rem" />} />
+          </AvatarGroup>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;

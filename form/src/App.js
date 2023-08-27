c38cb245ee42b16a1216a661c15530cf551927b6
Abhinav Singh <kittusinghranchi@gmail.com>
2023-08-28 02:23:02 +0530

    build version ready

--- form/src/App.js
+++ form/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import MainForm from "./components/MainForm/MainForm";
 import Footer from "./components/Footer";
-
+import "./App.css"
 const App = () => {
   return (
     <div className="">
